280f3a57e197121c5b7b60ac5153357e44f67391
Craig Thompson <craig@flodesign.co.uk>
2016-08-02 19:27:57 +0100

    Added Motoroutes namespace. Added Route nd Review models

---
+++ app/Motoroutes/Review.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\Motoroutes;
+
+
+class Review
+{
+    public function __construct()
+    {
+    }
+    
+    /**
+     * Relationships
+     */
+}
---
+++ app/Motoroutes/Route.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\Motoroutes;
+
+
+class Route
+{
+    public function __construct()
+    {
+    }
+    
+    /**
+     * Relationships
+     */
+}
